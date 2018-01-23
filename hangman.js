c6f8052a3784ba3f9a6f66b4b4df0dc192d42487
Greg Church <greggypc@users.noreply.github.com>
2018-01-22 21:53:50 -0600

    consolidate files to get code working

--- hangman.js
+++ hangman.js
@@ -37,3 +37,17 @@ var getAnotherLetter = function() {
 
 // call getAnotherLetter for initial run
 getAnotherLetter();
+
+var word = require("./word.js");
+
+function Letter (letter) {
+	this.letter = letter;
+	}
+
+// Now create a method for the constructor that would print all of the information contained within an object to the console. 
+// More memory efficient than putting
+//print function inside the object
+Letter.prototype.checkForLetter = function() {
+	console.log("checking if letter is in the word");
+	console.log("\n===================\n");
+	};
